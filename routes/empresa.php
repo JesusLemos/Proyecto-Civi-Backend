0e83cdeccb6263a32d645719826cb4711526bdfb
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-01-27 18:47:19 +0100

    Añadido el controlador VerSolicitudes de la vista empresa y funcionalidad

--- routes/empresa.php
+++ routes/empresa.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\PruebaController;
 Route::group(['middleware'=>['cors']], function (){
   //Registro, Login, Logout, Perfil, Modificar Perfil, Crear Ofertas
   Route::post('/crearoferta', 'CreacionOfertaController@CrearOferta');
+  Route::get('/versolicitudes/{id}', 'VerSolicitudesController@VerSolicitudesEmpresas');
 });
