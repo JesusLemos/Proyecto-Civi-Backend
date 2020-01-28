2fde54d7b704d5c469b2aac9f3b3cef25909b622
Jorge Vicens <gromit.jvv@gmail.com>
2020-01-28 12:20:14 +0100

    cambiados tipo de datos de tabla usuarios

--- database/migrations/2020_01_20_142737_create_usuarios_table.php
+++ database/migrations/2020_01_20_142737_create_usuarios_table.php
@@ -15,16 +15,16 @@ class CreateUsuariosTable extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('nombre_usuario', 25);
-            $table->string('apellido', 25);
-            $table->string('email');
-            $table->string('contrasenia');
-            $table->string('dni');
-            $table->longText('descripcion');
-            $table->string('telefono');
-            $table->string('direccion');
-            $table->longText('foto');
-            $table->dateTime('fecha_nacimiento');
+            $table->string('nombre_usuario', 25)->required();
+            $table->string('apellido', 25)->required();
+            $table->string('email')->unique()->required();
+            $table->string('contrasenia')->required();
+            $table->string('dni')->required();;
+            $table->longText('descripcion')->required();
+            $table->string('telefono')->required();
+            $table->string('direccion')->required();
+            $table->longText('foto')->required();
+            $table->dateTime('fecha_nacimiento')->required();
             $table->rememberToken();
             $table->timestamps();
         });
