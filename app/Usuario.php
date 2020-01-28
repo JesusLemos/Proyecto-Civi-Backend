8f27f16930e8f843851d73562b22b056a13dd848
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-01-28 09:48:15 +0100

    añadido campos en el registro de usuario

--- app/Usuario.php
+++ app/Usuario.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Usuario extends Model
 {
     //
+    
     public function solicitud(){
         return $this->belongsToMany('App\OfertaTrabajo', 'solicitudes','id', 'id_usuario')->withPivot('estado');
     }
