ebe3f633fc5225e3c9beccf55be520ae68677e94
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-01-28 09:21:54 +0100

    Correcion de datos

--- routes/empresa.php
+++ routes/empresa.php
@@ -5,4 +5,5 @@ Route::group(['middleware'=>['cors']], function (){
   //Registro, Login, Logout, Perfil, Modificar Perfil, Crear Ofertas
   Route::post('/crearoferta', 'CreacionOfertaController@CrearOferta');
   Route::get('/versolicitudes/{id}', 'VerSolicitudesController@VerSolicitudesEmpresas');
+  Route::get('/verperfil/{id}', 'VerPerfilController@verPerfilEmpresa');
 });
