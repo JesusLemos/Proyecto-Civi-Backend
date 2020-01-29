9402ff22d49fd6d78ff1adf52d3c9c438db082d0
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-01-29 09:10:27 +0100

    añadido el registro de usuario y validacion

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
@@ -47,28 +48,49 @@ class RegisterController extends Controller
      * @body  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator( $data)
-    {
-        $v = \Validator::make($data->all(), [
-        //  $prueba=$data->validate([
-            'nombre_usuario' => 'required|string|max:25',
-            'apellido' => 'required|string|max:25',
-            'email' => 'required|string|email|max:255|unique:usuarios',
-            'contrasenia' => 'required|string|min:8',
-            'dni' => 'required|string|max:255',
-            'descripcion' => 'required|string|max:255',
-            'telefono' => 'required|string|max:255',
-            'direccion' => 'required|string|max:255',
-            'foto' => 'required|string|max:255',
-            'fecha_nacimiento' => 'required|date',
-            'remember_token' =>  'string|max:255',
+//     protected function validator(Request $data)
+//     {
+//         try{
+
+     
+//         $v = \Validator::make($data->all(), [
+//         //  $prueba=$data->validate([
+//             'nombre_usuario' => 'required|string|max:25',
+//             'apellido' => 'required|string|max:25',
+//             'email' => 'required|string|email|max:255|unique:usuarios',
+//             'contrasenia' => 'required|min:8|string',
+//             'dni' => 'required|string|max:255',
+//             'descripcion' => 'required|string|max:255',
+//             'telefono' => 'required|string|max:255',
+//             'direccion' => 'required|string|max:255',
+//             'foto' => 'required|string|max:255',
+//             'fecha_nacimiento' => 'required|date',
+//             'remember_token' =>  'string|max:255',
            
-        ]);
-        if ($v->fails())
-        {
-             return 'algo fallo';
-        }
-    }
+//         ]);
+//     // return reponse($v);
+       
+//      }catch (Exception $e) {
+//         echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+//     }
+// }
+
+public function rules(){
+    return[
+        'nombre_usuario' => 'required|string|max:25',
+        'apellido' => 'required|string|max:25',
+        'email' => 'required|string|email|max:255|unique:usuarios',
+        'contrasenia' => 'required|min:8|string',
+        'dni' => 'required|string|max:255',
+        'descripcion' => 'required|string|max:255',
+        'telefono' => 'required|string|max:255',
+        'direccion' => 'required|string|max:255',
+        'foto' => 'required|string|max:255',
+        'fecha_nacimiento' => 'required|date',
+        // 'remember_token' =>  'string|max:255',
+    ];
+}
+
 
     /**
      * Create a new user instance after a valid registration.
@@ -78,39 +100,100 @@ class RegisterController extends Controller
      */
     protected function create(Request $data)
     {
-      if(validator([$data])){
-         
-        return Usuario::create([
-            'nombre_usuario'=>$data{"nombre_usuario"},
-            'apellido'=>$data{"apellido"},
-            'email'=>$data{"email"},
-            'dni'=>$data{"dni"},
-            'descripcion'=>$data{"descripcion"},
-            'telefono'=>$data{"telefono"},
-            'direccion'=>$data{"direccion"},
-            'foto'=>$data{"foto"},
-            'contrasenia'=>encrypt($data{"contrasenia"}),
-            'fecha_nacimiento'=>$data{"fecha_nacimiento"},
-            'remember_token'=>$data{"remember_token"}
-        ]
+        // try {
 
-         ); 
-      }else{
-        echo 'algo fallo';
-      }
-        //  return Usuario::create([
-        //     'nombre_usuario' => $data['nombre_usuario'],
-        //     'apellido'=>$data['apellido'],
-        //     'dni'=>$data['dni'],
-        //     'email' => $data['email'],
-        //     'descripcion'=>$data['descripcion'],
-        //     'telefono'=>$data['telefono'],
-        //     'direccion'=>$data['direccion'],
-        //     'foto'=>$data['foto'],
-        //     'contrasenia' => Hash::make($data['contrasenia']),
-        //     'fecha_nacimiento'=>$data['fecha_nacimiento'],
-        //     'remember_token'=>null,
+        //     $validator = \Validator::make($data->all(), [
+        //         'nombre_usuario' => ['required' , 'string','max:25', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'apellido' => ['required' , 'string','max:25', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'email' => ['required','string','email','max:255','unique:usuarios', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'contrasenia' => ['required','min:8','string', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'dni' => ['required','string','max:255', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'descripcion' => ['required','string','max:255', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'telefono' => ['required','string','max:255', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'direccion' => ['required','string','max:255', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'foto' => ['required','string','max:255', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }],
+        //         'fecha_nacimiento' => ['required','date', function($attribute, $value, $file){
+        //             if($value==='foo'){
+        //                 $fail($attribute.'is invalid');
+        //             }
+        //         }]
         //     ]);
-        // return $data{"nombre_usuario"};
+        try{
+            $data->validate([
+                'nombre_usuario' => 'required|string|max:25',
+                'apellido' => 'required|string|max:25',
+                'email' => 'required|string|email|max:255|unique:usuarios',
+                'contrasenia' => 'required|min:8|string',
+                'dni' => 'required|string|max:255',
+                'descripcion' => 'required|string|max:255',
+                'telefono' => 'required|string|max:255',
+                'direccion' => 'required|string|max:255',
+                'foto' => 'required|string|max:255',
+                'fecha_nacimiento' => 'required|date',
+                // 'remember_token' =>  'string|max:255',
+            ]);
+            return Usuario::create([
+                'nombre_usuario'=>$data{"nombre_usuario"},
+                'apellido'=>$data{"apellido"},
+                'email'=>$data{"email"},
+                'dni'=>$data{"dni"},
+                'descripcion'=>$data{"descripcion"},
+                'telefono'=>$data{"telefono"},
+                'direccion'=>$data{"direccion"},
+                'foto'=>$data{"foto"},
+                'contrasenia'=>$data{"contrasenia"},
+                'fecha_nacimiento'=>$data{"fecha_nacimiento"},
+                'remember_token'=>$data{"remember_token"}
+            ]
+    
+             );
+        } catch (\Exception $e) {
+          return $e->getMessage();
+        //    return ['code'=>$e->getCode(),
+        //    'file'=>$e->getFile(),
+        //    'line'=>$e->getLine(),
+        //    'message'=>$e->getMessage(),
+        //    'trace'=>$e->getTrace()];
+        // }catch(PDOException $e){
+
+        // }
+      
     }
+}
 }
