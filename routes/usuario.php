3c89fe47e351185536c496fef58b7cc4a8cee89d
JesusLemos <JesusLemosFoncubierta@gmail.com>
2020-01-31 17:42:50 +0100

    Añadido controlador logout de usuarios

--- routes/usuario.php
+++ routes/usuario.php
@@ -16,8 +16,9 @@ Route::group(['middleware'=>['cors']], function (){
   //Muestra el perfil de un usuario
   Route::get('/verperfil/{id}', 'VerPerfilController@verPerfilUsuario');
   Route::post('/modificarperfil', 'ModificarPerfilUsuarioController@CambiarPerfilUsuario');
-  Route::post('/borrarsolicitud', 'EliminarSolicitudUsuariosController@borrarSolicitud');
+
 });
+Route::post('/desconectarusuario', 'Auth\LogoutUsuarioController@DesconectarUsuario');
     //Se crea un usuario
   Route::post('/registro', 'Auth\RegisterUsuarioController@create');
   //Se logea un usuario
